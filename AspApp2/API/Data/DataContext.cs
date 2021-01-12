d14ff7606407662f94288f052eebc308381158a6
colin detwiller <colindetwillerjr@gmail.com>
2021-01-12 16:38:30 -0500

    1-12 +was stuck setting up workspace, took an hour

---
+++ AspApp2/API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using AspApp2.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
