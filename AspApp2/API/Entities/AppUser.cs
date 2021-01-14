1d8ba0da6f6155d85358c7d6c6dc435913abeb42
colin detwiller <colindetwillerjr@gmail.com>
2021-01-14 13:22:03 -0500

    4-34 added DB user password hash and salt

--- AspApp2/API/Entities/AppUser.cs
+++ AspApp2/API/Entities/AppUser.cs
@@ -4,6 +4,7 @@ namespace AspApp2.API.Entities
     {
         public int Id { get; set; }
         public int UserName { get; set; }
-        
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
